cce6949fc05d09074e86f058b8d29a4bf45b8cad
Tac <taylorcryer84@gmail.com>
2024-12-22 15:17:03 -0600

    Fixed Layout!!

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,21 @@ import Link from "next/link";
 import { homePath, ticketsPath } from "@/paths";
 import localFont from "next/font/local";
 
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
 export const metadata: Metadata = {
   title: "TAC",
   description: "My Road to Next app",
 };
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -18,13 +27,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <nav 
-        className= "
-        supports-backdrop-blur:bg-background/60
-        fixed left-0 right-0 top-0 z-20
-        border-b bg-background/95 backdrop-blur
-        w-full flex py-2.5 px-5 justify between
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <nav
+          className="
+            supports-backdrop-blur:bg-background/60
+            fixed left-0 right-0 top-0 z-20
+            border-b bg-background/95 backdrop-blur
+            w-full flex py-2.5 px-5 justify-between
         "
         >
           <div>
@@ -37,17 +48,18 @@ export default function RootLayout({
               Tickets
             </Link>
           </div>
-          </nav>
-          <main className= "
+        </nav>
+        <main
+          className="
               min-h-screen flex-1
               overflow-y-auto overflow-x-hidden
               py-24 px-8
               bg-secondary/20
-              flex flex-col ;
+              flex flex-col
               "
-              >
-            {children}
-          </main>
+        >
+          {children}
+        </main>
       </body>
     </html>
   );
