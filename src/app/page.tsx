c77d6fb895b9fb86a6421ec43a364e39c30adc31
Tac <taylorcryer84@gmail.com>
2025-05-31 19:35:06 -0500

    Added tailwind css stylings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,19 @@ import Link from "next/link";
 
 const HomePage = () => {
   return (
-    <div>
-      <h2>HomePage</h2>
+    <div className="flex-1 flex flex-col gap-y-8">
+      <div>
+        <h2 className="text-3xl font-bold tracking-tight">HomePage</h2>
+        <p className="text-sm text-muted-foreground">
+          Your home place to start
+        </p>
+      </div>
 
-      <Link href="/tickets" className="underline">
-        Go to Tickets
-      </Link>
+      <div className="flex-1 flex flex-col items-center">
+        <Link href="/tickets" className="underline">
+          Go to Tickets
+        </Link>
+      </div>
     </div>
   );
 };
