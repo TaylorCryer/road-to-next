33e75f6b3b23914de4b9c510350acd88067d9c71
Tac <taylorcryer84@gmail.com>
2025-06-02 01:44:10 -0500

    Added css stylings to HomePage & TicketsPage and added conditional styling to tickets

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import Link from "next/link";
 import { initialTickets } from "@/data";
 import { ticketPath } from "@/paths";
@@ -10,17 +11,36 @@ const TICKETS_ICON = {
 
 const TicketsPage = () => {
   return (
-    <div>
-      {initialTickets.map((ticket) => (
-        <div key={ticket.id}>
-          <div>{TICKETS_ICON[ticket.status]}</div>
-          <h2 className="text-lg">{ticket.title}</h2>
+    <div className="flex-1 flex flex-col gap-y-8">
+      <div>
+        <h2 className="text-3xl font-bold tracking-tight">TicketsPage</h2>
+        <p className="text-sm text-muted-foreground">
+          All your tickets at one place
+        </p>
+      </div>
 
-          <Link href={ticketPath(ticket.id)} className="text-sm underline">
-            View
-          </Link>
-        </div>
-      ))}
+      <div className="flex-1 flex flex-col items-center gap-y-4">
+        {initialTickets.map((ticket) => (
+          <div
+            key={ticket.id}
+            className="w-full max-w-[420px] p-4 border border-slate-100 rounded"
+          >
+            <div>{TICKETS_ICON[ticket.status]}</div>
+            <h3 className="text-lg font-semibold truncate">{ticket.title}</h3>
+            <p
+              className={clsx("text-sm text-slate-500 truncate", {
+                "line-through": ticket.status === "DONE",
+              })}
+            >
+              {ticket.content}
+            </p>
+
+            <Link href={ticketPath(ticket.id)} className="text-sm underline">
+              View
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
