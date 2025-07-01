3d932937c6d8e353f7253a37c487ace9860f3a64
Taylor Cryer <159663917+TaylorCryer@users.noreply.github.com>
2025-07-01 10:14:08 -0500

    Macbook tailwind css (#8)
    * Added css stylings to HomePage & TicketsPage and added conditional styling to tickets
    
    * Added styling to rootLayout, created animation in tailwind, and implimented in TicketsPage
    
    * Added actual Icons to the ticketIcons via SVGs
    
    * updates
    
    * Installed ShadCN
    
    ---------
    
    Co-authored-by: Tac <taylorcryer84@gmail.com>

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,11 +1,63 @@
+import clsx from "clsx";
 import Link from "next/link";
 import { initialTickets } from "@/data";
 import { ticketPath } from "@/paths";
 
+const CheckIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="size-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="m4.5 12.75 6 6 9-13.5"
+    />
+  </svg>
+);
+
+const PencilIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="size-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125"
+    />
+  </svg>
+);
+
+const DocumentIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="size-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
+    />
+  </svg>
+);
+
 const TICKETS_ICON = {
-  OPEN: "O",
-  IN_PROGRESS: ">",
-  DONE: "X",
+  OPEN: <DocumentIcon />,
+  IN_PROGRESS: <PencilIcon />,
+  DONE: <CheckIcon />,
 };
 
 const TicketsPage = () => {
@@ -14,11 +66,11 @@ const TicketsPage = () => {
       <div>
         <h2 className="text-3xl font-bold tracking-tight">TicketsPage</h2>
         <p className="text-sm text-muted-foreground">
-          All of your tickets at one place
+          All your tickets at one place
         </p>
       </div>
 
-      <div className="flex-1 flex flex-col items-center gap-y-4">
+      <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-in-from-top">
         {initialTickets.map((ticket) => (
           <div
             key={ticket.id}
@@ -26,10 +78,16 @@ const TicketsPage = () => {
           >
             <div>{TICKETS_ICON[ticket.status]}</div>
             <h3 className="text-lg font-semibold truncate">{ticket.title}</h3>
-            <p className="text-sm text-slate-500 truncate">{ticket.content}</p>
+            <p
+              className={clsx("text-sm text-slate-500 truncate", {
+                "line-through": ticket.status === "DONE",
+              })}
+            >
+              {ticket.content}
+            </p>
 
             <Link href={ticketPath(ticket.id)} className="text-sm underline">
-              VIEW
+              View
             </Link>
           </div>
         ))}
